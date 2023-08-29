5215ce3c457375b5851b3f370946d6161f923588
hoscho <jgeist@gmx.net>
2023-08-29 16:27:23 +0200

    Initial commit

---
+++ src/main/java/de/jgsoftwares/sipphone/asteriksipphone/Asteriksipphone.java
@@ -0,0 +1,13 @@
+
+package de.jgsoftwares.sipphone.asteriksipphone;
+
+/**
+ *
+ * @author hoscho
+ */
+public class Asteriksipphone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
